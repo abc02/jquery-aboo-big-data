47e5f1a99e91f84009a349f58c138ad7b20468bd
abc02 <abcgg02@gmail.com>
2018-09-13 16:57:07 +0800

    update 20180913 index.html add trajectory.html`

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@ $('.conctrol-button').click(function () {
   $('.slider-dialog').toggle()
 })
 $('.slider-doork').click(function () {
-  $('.slider-dialog').hide()
+  $('.slider-dialog').toggle()
 })
 $('.slider-close').click(function () {
   $('.slider-dialog').hide()
@@ -22,28 +22,7 @@ $('.bottom-close').click(function () {
   $('.bottom-dialog').hide()
   $('.bottom-doork').show()
 })
-//百度地图API功能
-var map = new BMap.Map("allmap");            // 创建Map实例
-var point = new BMap.Point(120.618679, 28.028974); // 创建点坐标
-map.centerAndZoom(point, 12);
-map.enableScrollWheelZoom();                 //启用滚轮放大缩小
-map.enableInertialDragging();
 
-map.enableContinuousZoom();
-
-var size = new BMap.Size(10, 20);
-map.addControl(new BMap.CityListControl({
-    anchor: BMAP_ANCHOR_TOP_LEFT,
-    offset: size,
-    // 切换城市之间事件
-    onChangeBefore: function(){
-       console.log('before');
-    },
-    // 切换城市之后事件
-    onChangeAfter:function(){
-      console.log('after');
-    }
-}));
 function financial(x) {
   return Number.parseFloat(x).toFixed(4);
 }
@@ -90,3 +69,71 @@ function timestamp(date, isType) {
 axios.defaults.baseURL = 'https://datainterface.abpao.com/v1/xiedian_data'
 axios.defaults.headers.common['Authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBZG1pbklkIjoiMyIsImV4cCI6MTUzODI4OTgwM30.YkXr2kW9F58d0Nt9EjHLp1NgHoTN4Ykg7iqTHYzMiug'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
+//百度地图API功能
+var map = (function (BMap) {
+  let map = new BMap.Map("allmap");            // 创建Map实例
+  let point = new BMap.Point(116.331398, 39.897445); // 默认北京
+  let currentCity = new BMap.LocalCity();
+  let geoc = new BMap.Geocoder(); // 类用于获取用户的地址解析。
+  let cityName
+  map.centerAndZoom(point, 12);
+  map.enableContinuousZoom(); // 启用连续缩放效果，默认禁用
+  map.enableScrollWheelZoom();  // 启用滚轮放大缩小
+  map.enableInertialDragging(); // 启用地图惯性拖拽，默认禁用
+  currentCity.get(function (result) {
+    cityName = result.name
+    map.setCenter(cityName)
+  })
+  // map.addEventListener("tilesloaded", function () {//地图加载完毕
+  //   let visibleMakers, makers, bs, bssw, bsne, b
+  //   makers = map.getOverlays()
+  //   bs = map.getBounds();   //获取可视区域
+  //   bssw = bs.getSouthWest();   //可视区域左下角
+  //   bsne = bs.getNorthEast();   //可视区域右上角
+  //   b = new BMap.Bounds(new BMap.Point(bssw.lng, bssw.lat),new BMap.Point(bsne.lng, bsne.lat));
+  //   visibleMakers = makers.map(maker => {
+  //     if(b.containsPoint(maker.getPosition())) {
+  //       return maker
+  //     }
+  //   })
+  //   console.log(visibleMakers.length)
+  //   $('.visible-maker').text(visibleMakers.length)
+  //   //加载可视区域内已保存的标点
+  //   //略...
+  // });
+  // //鼠标点击触发事件
+  // map.addEventListener("click", function (e) {
+  //   var pt = e.point; //获取标点
+  //   geoc.getLocation(pt, function (rs) {
+  //     //返回地址描述信息，是个字符串，比如：北京市丰台区海鹰路9号
+  //     var add = rs.address;
+
+  //     //返回结构化的地址描述信息，是个对象，返回省、市、县等信息
+  //     var addComp = rs.addressComponents;
+  //     //alert(addComp.province + ", " + addComp.city + ", " + addComp.district + ", " + addComp.street + ", " + addComp.streetNumber);
+
+  //     //判断标点所属城市，非本城市不允许标点
+  //     if (addComp.city != cityName) {
+  //       alert('只能在本城市范围内标点');
+  //       return false;
+  //     }
+  //   });
+  // })
+
+  let size = new BMap.Size(10, 20);
+  map.addControl(new BMap.CityListControl({
+    anchor: BMAP_ANCHOR_TOP_LEFT,
+    offset: size,
+    // 切换城市之间事件
+    onChangeBefore: function () {
+      console.log('before');
+    },
+    // 切换城市之后事件
+    onChangeAfter: function (e) {
+      console.log(map.getCurrentCity())
+      console.log('after');
+    }
+  }));
+  return map
+}(BMap))
