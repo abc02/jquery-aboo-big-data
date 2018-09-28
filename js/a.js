70ed2bdfec85715cf4d7b921a66ffb91c00268a1
abc02 <abcgg02@gmail.com>
2018-09-28 10:58:06 +0800

    update 20180928 update-file ecxl

--- js/a.js
+++ js/a.js
@@ -853,17 +853,20 @@ let a = (function (map) {
     oldliveinfo = null,
     isFirstGetLastPosition = true,
     intervalerGetLastPosition = null,
-    currentData = new Date()
-  $('#excel-file').change(function (e) {
-    var files = e.target.files;
-    var fileReader = new FileReader();
+    currentData = new Date(),
+    persons = []  // 存储获取到的数据
+  function handlerFile(e) {
+    let files = e.target.files,
+      fileName = files[0].name,
+      fileSize = `大小：${(files[0].size / 1024).toFixed(0)}kb`
+    fileReader = new FileReader()
     fileReader.onload = function (ev) {
       try {
         var data = ev.target.result,
           workbook = XLSX.read(data, {
             type: 'binary'
-          }), // 以二进制流方式读取得到整份excel表格对象
-          persons = []; // 存储获取到的数据
+          }) // 以二进制流方式读取得到整份excel表格对象
+          // persons = []; // 存储获取到的数据
       } catch (e) {
         console.log('文件类型不正确');
         return;
@@ -871,6 +874,7 @@ let a = (function (map) {
 
       // 表格的表格范围，可用于判断表头是否数量是否正确
       var fromTo = '';
+      console.log(workbook)
       // 遍历每张表读取
       for (var sheet in workbook.Sheets) {
         if (workbook.Sheets.hasOwnProperty(sheet)) {
@@ -890,11 +894,22 @@ let a = (function (map) {
           break; // 如果只取第一张表，就取消注释这行
         }
       }
+      $('.file-name').text(fileName)
+      $('.file-size').text(fileSize)
+      $('.file-result').show()
       console.log(persons);
     };
     // 以二进制方式打开文件
     fileReader.readAsBinaryString(files[0]);
-  });
+  }
+  $('#excel-file').on('change', handlerFile);
+  $('#restart-file').on('change', handlerFile);
+  $('#update-file').on('click', function() {
+    let userinfo = a._GetLoaclUserInfo()
+    a.BatchAddFixing({ adminId: userinfo.AdminId, batchId: '', fixingIds: persons.join(',')}).then(res => {
+      alert(res.code)
+    })
+  })
   $LOGIN_FORM.submit(function (e) {
     e.preventDefault()
     let $currentTarget = $(e.currentTarget),
@@ -1150,6 +1165,12 @@ let a = (function (map) {
       return res.data
     })
   }
+  function BatchAddFixing({ adminId, batchId, fixingIds }) {
+    return axios.post('/BatchAddFixing', Qs.stringify({ adminId, batchId, fixingIds})).then(res => {
+      if (!res) return
+      return res.data
+    })
+  }
   function _SetLoaclStorageUserInfo(userInfo) {
     window.localStorage.setItem('userinfo', JSON.stringify(userInfo))
   }
@@ -1223,6 +1244,7 @@ let a = (function (map) {
     AdminGetInstructionsList,
     AdminGetInstructions,
     GetFixingListForSearch,
+    BatchAddFixing,
     GetCurrentData,
     GetLoaclStorageUserInfo,
     ClearLoaclStorageUserInfo,
