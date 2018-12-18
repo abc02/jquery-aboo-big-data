94f394ed5a57b9775728b1a6592e8d33b46e7c39
abc02 <abcgg02@gmail.com>
2018-12-18 13:57:21 +0800

    update modifypasswd

--- js/header.js
+++ js/header.js
@@ -7,8 +7,29 @@ var header = (function ($el) {
 
   return {
     refresh(userInfo) {
-      $el.html(`<img src="/assets/default.png" wdith="48" height="48" /><span class="ml-2 mr-2">${userInfo.UserName}</span> <span class="point logout">注销</span>`)
+      $el.html(`<img src="/assets/default.png" wdith="48" height="48" /><span class="ml-2 mr-2">${userInfo.UserName}</span> <span class="mr-2 point modiyfPasswd">修改密码</span> <span class="point logout">注销</span>`)
         .off('click')
+        .on('click', '.modiyfPasswd', function (e) {
+          $('#modifypasswd-ModalCenter').off('click').on('click', '.modifypasswd-button', function (e) {
+            let oldPassword = $('#modifypasswd-ModalCenter').find('#oldPasswd').val()
+            let newPassword = $('#modifypasswd-ModalCenter').find('#newPasswd').val()
+            let checkNewPassword = $('#modifypasswd-ModalCenter').find('#checkNewPasswd').val()
+            if(!oldPassword) return alert('旧密码不能为空')
+            if(!newPassword) return alert('新密码不能为空')
+            if(!checkNewPassword) return alert('再次输入密码不能为空')
+            if(newPassword !== checkNewPassword) return alert('再次输入密码不一致')
+            var userInfo = utils.GetLoaclStorageUserInfo('userinfo')
+            LOGIN_API.AdminEditPassword({ adminId: userInfo.AdminId, oldPassword, newPassword }).then(res => {
+              alert(res.data.code)
+              if (res.data.ret === 1001) {
+                $('#modifypasswd-ModalCenter').modal('hide')
+                utils.DelLoaclStorageUserInfo('userinfo')
+                Event.create('login').trigger('logOut', 'login')
+              }
+            })
+          })
+          $('#modifypasswd-ModalCenter').modal('show')
+        })
         .on('click', '.logout', function (e) {
           if(!window.confirm('确定登出账号吗？')) return
           
